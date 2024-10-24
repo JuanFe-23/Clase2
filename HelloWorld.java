5d5b17cd9d3c59292c264758c16c72297b88ff2a
Juan Linares <juanzlinares29@gmail.com>
2024-10-23 23:00:40 -0500

    Mi primer Programa

---
+++ HelloWorld.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+       
+        
+        System.out.print("Hola!");
+        System.out.print("\n¿Como estás?");
+
+        System.out.printf("\nMi nombre es %s y tengo %d años","Juan Linares", 23);
+
+
+        /*Scanner sc = new Scanner(System.in);
+        String name;
+
+        System.out.print("\nDigite su nombre: ");
+        name = sc.nextLine();
+        
+        System.out.println("Hola "+name + " ¿Como estás?");
+        
+        sc.close();
+        */
+
+
+        
+    }
+}
